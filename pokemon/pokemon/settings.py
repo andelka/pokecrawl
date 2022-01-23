cdd04102ab21d9bd5e385760d6087d3532103872
andelka <a.tesarova@gmail.com>
2022-01-23 20:49:05 +0100

    update settings

--- pokemon/pokemon/settings.py
+++ pokemon/pokemon/settings.py
@@ -11,7 +11,8 @@ BOT_NAME = 'pokemon'
 
 SPIDER_MODULES = ['pokemon.spiders']
 NEWSPIDER_MODULE = 'pokemon.spiders'
-
+FEED = 'json'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'pokemon (+http://www.yourdomain.com)'
@@ -20,12 +21,12 @@ NEWSPIDER_MODULE = 'pokemon.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 3
+#CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+#DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
